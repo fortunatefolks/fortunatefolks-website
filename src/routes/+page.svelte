867b008b5cad553f4b609d6c1cf8b3c88bf787d1
aqeelchishti <git@github.com>
2023-03-12 22:14:18 +0500

    30th update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,7 +7,7 @@
 >
 	<div class="max-w-2xl mx-auto px-2 flex flex-wrap align-middle">
 		<div class="max-w-md mx-auto">
-			<h2 class="text-center font-bold text-3xl align-middle text-[#2675ac]">
+			<h2 class="text-center font-bold text-3xl align-middle text-gray-900">
 				We are a non-profit organization to provide entrepreneurship services for economic
 				development in lagged areas
 			</h2>
