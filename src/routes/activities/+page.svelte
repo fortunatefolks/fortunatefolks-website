aa9069f5a91a50507c586188c81c8862d30c7a97
aqeelchishti <git@github.com>
2023-03-12 18:32:17 +0500

    14th update

--- src/routes/activities/+page.svelte
+++ src/routes/activities/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import video1 from '$lib/assets/gallery/video1.mp4';
 	import image2 from '$lib/assets/gallery/image2.jpeg';
 	import image3 from '$lib/assets/gallery/image3.jpeg';
 	import image4 from '$lib/assets/gallery/image4.jpeg';
@@ -16,6 +17,11 @@
 	<h2 class="mt-4 mb-4 text-3xl font-bold leading-tight align-middle text-center">Activities</h2>
 	<div class="container mx-auto px-5 py-2 lg:px-32 lg:pt-12">
 		<div class="-m-1 flex flex-wrap md:-m-2">
+			<div class="flex w-full flex-wrap">
+				<video class="w-full h-auto max-w-full p-1 md:p-2 rounded-lg" controls>
+					<source class="rounded-lg" src={video1} type="video/mp4" />
+				</video>
+			</div>
 			{#each images_list as image}
 				<div class="flex w-1/2 flex-wrap">
 					<div class="w-full p-1 md:p-2">
